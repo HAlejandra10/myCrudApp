b033c327b84703492ef087dcec18cf8a10406f2e
HeidyAPrimo <heidyprimo10@outlook.com>
2022-05-17 21:58:29 -0500

    rename funtion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import React from 'react';
 import './App.css';
 import Listar from './componentes/listar';
 
 function App() {
   return (
-   
     <div className="App">
         Hola mundo!
         <Listar></Listar>
