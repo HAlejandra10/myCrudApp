b98be994084b18349a1bab9e1aa2edf63f3f523e
HeidyAPrimo <heidyprimo10@outlook.com>
2022-05-17 21:05:41 -0500

    otro cambio

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import Listar from './componentes/listar';
-import {Route, BrowserRouter as Router} from 'react';
 
 function App() {
   return (
    
     <div className="App">
         Hola mundo!
-        {/* <Route exact path="/" component={Listar}> </Route> */}
+        <Listar></Listar>
     </div>
   );
 }
-
 export default App;
